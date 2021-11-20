474d6c9d15fb8181e139d62e03e4dc261618b175
unknown <caryhuit@gmail.com>
2021-11-20 22:49:50 +0800

    semi-final commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="container-fluid">
+    <div class="d-flex flex-wrap justify-content-center">
+        <div class="d-flex flex-row">
+            <img src="https://www.bing.com/images/search?view=detailV2&ccid=PNx4dIev&id=69BFFEBA0EDF7BD8559ECA4EF9D0B4DEEB4FD982&thid=OIP.PNx4dIevdhcGR3elkJJEjgHaEK&mediaurl=https%3a%2f%2f4kwallpapers.com%2fimages%2fwallpapers%2fbugatti-chiron-pur-sport-bugatti-chiron-super-sport-300-2560x1440-2574.jpeg&exph=1440&expw=2560&q=cars+image&simid=607986800283632720&FORM=IRPRST&ck=690C64BD39568188711DF5CE15C3FF60&selectedIndex=15" class="img-fluid" alt="connect to internet to view the image">
+        </div>
+
+    </div>
+</div>
+
+
+@endsection
